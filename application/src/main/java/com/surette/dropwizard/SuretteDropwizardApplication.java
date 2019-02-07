1d2111a1a780092ba43ae3d58d8de455ee9c07f1
Josh <surettej@bu.edu>
2019-02-06 22:48:02 -0500

    configuring logging

--- application/src/main/java/com/surette/dropwizard/SuretteDropwizardApplication.java
+++ application/src/main/java/com/surette/dropwizard/SuretteDropwizardApplication.java
@@ -1,10 +1,16 @@
 package com.surette.dropwizard;
 
 import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SuretteDropwizardApplication extends Application<SuretteDropwizardConfiguration> {
+
+    Logger logger = LoggerFactory.getLogger(SuretteDropwizardApplication.class);
 
     public static void main(final String[] args) throws Exception {
         new SuretteDropwizardApplication().run(args);
@@ -17,11 +23,17 @@ public class SuretteDropwizardApplication extends Application<SuretteDropwizardC
 
     @Override
     public void initialize(final Bootstrap<SuretteDropwizardConfiguration> bootstrap) {
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(
+                        bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(false)
+                )
+        );
     }
 
     @Override
     public void run(final SuretteDropwizardConfiguration configuration,
                     final Environment environment) {
-        // TODO: implement application
+        logger.info("DUMMY");
     }
 }
